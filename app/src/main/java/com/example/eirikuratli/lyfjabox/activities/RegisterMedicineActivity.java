acc0ed46ef056e5f284f1e62c559fe557dbf85e9
Þórunn <thorunn.thorarins@gmail.com>
2017-03-25 19:41:38 +0000

    Added some activities

---
+++ app/src/main/java/com/example/eirikuratli/lyfjabox/activities/RegisterMedicineActivity.java
@@ -0,0 +1,13 @@
+package com.example.eirikuratli.lyfjabox.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_medicine);
+    }
+}
