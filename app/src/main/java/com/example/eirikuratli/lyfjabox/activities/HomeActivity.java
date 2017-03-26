5c89ceed9bad8077f7b9bab923816d5311a4fcab
Þórunn <thorunn.thorarins@gmail.com>
2017-03-26 22:36:31 +0000

    minor changes

--- app/src/main/java/com/example/eirikuratli/lyfjabox/activities/HomeActivity.java
+++ app/src/main/java/com/example/eirikuratli/lyfjabox/activities/HomeActivity.java
@@ -1,14 +1,16 @@
 package com.example.eirikuratli.lyfjabox.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.eirkuratli.lyfjabox.R;
 
 public class HomeActivity extends AppCompatActivity {
 
-    //TODO: Create methods for buttons, link them to other activites
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,53 @@ public class HomeActivity extends AppCompatActivity {
         TextView greetingText = (TextView) findViewById(R.id.greeting_user);
         String format = getString(R.string.greeting_home);
         greetingText.setText(String.format(format, name));
+
+        Button mSearch = (Button) findViewById(R.id.search_meds_button);
+        mSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button mFindPharmacy = (Button) findViewById(R.id.search_pharmacy_button);
+        mFindPharmacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        //Fix with userinfoactivity class when pushed
+        Button mUserInfo = (Button) findViewById(R.id.user_information_button);
+        mUserInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button mViewMeds = (Button) findViewById(R.id.registered_meds_button);
+        mViewMeds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MedicineOverviewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//TODO: Create logout method, this will only direct the user to the WelcomeActivity but not log out
+        TextView mLogOut = (TextView) findViewById(R.id.log_out);
+        mLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
