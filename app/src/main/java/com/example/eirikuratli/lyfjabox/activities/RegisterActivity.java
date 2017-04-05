927f076a7ca7d4b237d5a484c769e05621f3f8c1
eikibriem <eikibriem@hotmail.com>
2017-04-05 18:26:16 +0000

    Registration complete

--- app/src/main/java/com/example/eirikuratli/lyfjabox/activities/RegisterActivity.java
+++ app/src/main/java/com/example/eirikuratli/lyfjabox/activities/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.eirikuratli.lyfjabox.activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -14,14 +15,23 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.eirikuratli.lyfjabox.R;
+import com.example.eirikuratli.lyfjabox.models.LoginConnection;
+import com.example.eirikuratli.lyfjabox.models.LoginUser;
 import com.example.eirikuratli.lyfjabox.models.RegisterConnection;
 import com.example.eirikuratli.lyfjabox.models.User;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "RegisterActivity";
     private EditText mFirstName, mLastName, mSocial, mAddress, mUsername, mEmail, mPassword, mConfirmedPassword, mPhoneNo;
     private Spinner mZipSpinner;
     private Button mRegister;
+    private User newUser;
+
+    private UserRegistrationTask mAuthTask = null;
 
     SharedPreferences shared;
 
@@ -59,6 +69,139 @@ public class RegisterActivity extends AppCompatActivity {
         mRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                attemptRegistration();
+            }
+        });
+    }
+
+    private void attemptRegistration() {
+        if (mAuthTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmail.setError(null);
+        mPassword.setError(null);
+
+        // Store values at the time of the login attempt.
+        newUser = createUser();
+
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        /*
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+        */
+
+        /*
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            //mAuthTask = new UserLoginTask(email, password, this);
+            //mAuthTask.execute((LoginUser) null);
+            AsyncTask task = new WelcomeActivity.UserLoginTask().execute(loginUser);
+        }
+        */
+        AsyncTask task = new RegisterActivity.UserRegistrationTask().execute(newUser);
+    }
+
+
+        //mLoginFormView = findViewById(R.id.registration_form);
+        //mProgressView = findViewById(R.id.registration_progress);
+
+    /**
+     * Represents an asynchronous login/registration task used to authenticate
+     * the user.
+     */
+    public class UserRegistrationTask extends AsyncTask<User, Void, String> {
+
+        /*private final String mEmail;
+        private final String mPassword;
+        private final Context mContext;
+
+        UserLoginTask(String email, String password, Context context) {
+            mEmail = email;
+            mPassword = password;
+            mContext = context;
+        }
+*/
+        @Override
+        protected String doInBackground(User... Users) {
+            // TODO: attempt authentication against a network service.
+
+            if(Users.length>0) {
+                Log.i(TAG, Users.length+" er lengdin á Users " );
+                Log.i(TAG, Users.toString());
+                String message = new RegisterConnection().sendRegistration(Users[0]);
+                return message;
+            }
+            return "Doesnt work, there is no user";
+        }
+
+        @Override
+        protected void onPostExecute(String response){
+            Log.d(TAG, response.toString());
+            if("OK".equals(response)) {
+
+                shared =getSharedPreferences("UserInfo", MODE_PRIVATE);
+
+                SharedPreferences.Editor editor = shared.edit();
+                editor.putString("firstname", newUser.getFirstName());
+                editor.putString("lastname", newUser.getLastName());
+                editor.putString("address", newUser.getAddress());
+                editor.putString("city", newUser.getCity());
+                editor.putString("email", newUser.getEmail());
+                editor.putString("password", newUser.getPassword());
+                editor.putInt("phoneNo", newUser.getPhoneNo());
+                editor.putInt("ssn", newUser.getSocial());
+                editor.putInt("zip", newUser.getZip());
+                editor.putString("username", newUser.getUsername());
+                editor.commit();
+
+                Toast toast = Toast.makeText(RegisterActivity.this ,"registration successful", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                Log.d(TAG, "GilmoreGirlsÁHomeScreen: " + mEmail.getText().toString());
+                intent.putExtra("email", mEmail.getText().toString());
+                startActivity(intent);
+            }
+            else {
+                Log.e(TAG, "Error when handling json");
+                Toast toast = Toast.makeText(RegisterActivity.this, "Sign up not successful, try again later", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+
+            }
+        }
+    }
+
+/*
                 mPassword.setError(null);
 
                 User newUser = createUser();
@@ -105,7 +248,7 @@ public class RegisterActivity extends AppCompatActivity {
                     toast.setGravity(Gravity.CENTER, 0, 0);
                     toast.show();
                 }
-                if (message.contains("complete")) {
+                if (message.equals("OK")) {
                     shared =getSharedPreferences("UserInfo", MODE_PRIVATE);
 
                     SharedPreferences.Editor editor = shared.edit();
@@ -134,9 +277,11 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
-    }
+        */
+
     public User createUser() {
         User newUser = new User();
+        newUser.setUsername(mEmail.getText().toString());
         newUser.setFirstName(mFirstName.getText().toString());
         newUser.setLastName(mLastName.getText().toString());
         newUser.setEmail(mEmail.getText().toString());
--- app/src/main/java/com/example/eirikuratli/lyfjabox/models/RegisterConnection.java
+++ app/src/main/java/com/example/eirikuratli/lyfjabox/models/RegisterConnection.java
@@ -2,6 +2,12 @@ package com.example.eirikuratli.lyfjabox.models;
 
 import android.net.Uri;
 import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import com.google.gson.Gson;
 
